0d36218f14075226c76f20bae1437e9337634bb1
Sverovich <developer@serverasamac.local>
2017-07-23 13:56:11 -0500

    creación de objeto persona

---
+++ ProjSw0/PersonaObject.swift
@@ -0,0 +1,13 @@
+//
+//  PersonaObject.swift
+//  ProjSw0
+//
+//  Created by Sverovich on 23/07/17.
+//  Copyright © 2017 Asael Virgilio. All rights reserved.
+//
+
+import UIKit
+
+class PersonaObject: NSObject {
+
+}

298298cab6bf33970e50bde4a503f7d30d2f8e50
Sverovich <developer@serverasamac.local>
2017-07-23 13:47:36 -0500

    Initial Commit

---
+++ ProjSw0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjSw0
+//
+//  Created by Sverovich on 23/07/17.
+//  Copyright © 2017 Asael Virgilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
